8e700f5bc1038581aad01552e1f69cac8714d33e
Burak Gürsoy <burak@cpan.org>
2009-08-27 06:10:17 +0300

    migrate from qv() to version->new. move accessors into base class

--- lib/Parse/HTTP/UserAgent.pm
+++ lib/Parse/HTTP/UserAgent.pm
@@ -8,6 +8,7 @@ use base qw(
     Parse::HTTP::UserAgent::IS
     Parse::HTTP::UserAgent::Parsers
     Parse::HTTP::UserAgent::Dumper
+    Parse::HTTP::UserAgent::Accessors
 );
 use overload '""',    => 'name',
              '0+',    => 'version',
@@ -42,58 +43,6 @@ sub new {
     $self->_parse;
     $self;
 }
-
-#------------------------------------------------------------------------------#
-
-sub name    { shift->[UA_NAME]    || '' }
-sub unknown { shift->[UA_UNKNOWN] || '' }
-sub generic { shift->[UA_GENERIC] || '' }
-sub os      { shift->[UA_OS]      || '' }
-sub lang    { shift->[UA_LANG]    || '' }
-sub robot   { shift->[UA_ROBOT]   || 0  }
-
-sub original_name    { shift->[UA_ORIGINAL_NAME]    || '' }
-sub original_version { shift->[UA_ORIGINAL_VERSION] || '' }
-
-sub version {
-    my $self = shift;
-    my $type = shift || '';
-    return $self->[ $type eq 'raw' ? UA_VERSION_RAW : UA_VERSION ] || 0;
-}
-
-sub mozilla {
-    my $self = shift;
-    return +() if ! $self->[UA_MOZILLA];
-    my @rv = @{ $self->[UA_MOZILLA] };
-    return wantarray ? @rv : $rv[0];
-}
-
-sub toolkit {
-    my $self = shift;
-    return +() if ! $self->[UA_TK];
-    return @{ $self->[UA_TK] };
-}
-
-sub extras {
-    my $self = shift;
-    return +() if ! $self->[UA_EXTRAS];
-    return @{ $self->[UA_EXTRAS] };
-}
-
-sub dotnet {
-    my $self = shift;
-    return +() if ! $self->[UA_DOTNET];
-    return @{ $self->[UA_DOTNET] };
-}
-
-#TODO: new accessors
-#strength
-#wap
-#mobile
-#parser
-#device
-
-#------------------------------------------------------------------------------#
 
 sub as_hash {
     my $self   = shift;
@@ -239,7 +188,7 @@ sub _numify {
                  $w !~ m{Version string .+? contains invalid data; ignoring:};
         warn $w if $ok;
     };
-    my $rv = qv($v)->numify;
+    my $rv = version->new("$v")->numify;
     return $rv;
 }
 
