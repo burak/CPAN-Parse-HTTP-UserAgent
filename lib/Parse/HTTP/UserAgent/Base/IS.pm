d94d9d98028a15a9211c609b451c5174e494d829
Burak Gürsoy <burak@cpan.org>
2009-08-29 09:02:35 +0300

    increase readability

--- lib/Parse/HTTP/UserAgent/Base/IS.pm
+++ lib/Parse/HTTP/UserAgent/Base/IS.pm
@@ -7,7 +7,7 @@ $VERSION = '0.10';
 
 sub _is_opera_pre {
     my($self, $moz) = @_;
-    return index( $moz, "Opera") != -1;
+    return index( $moz, 'Opera') != -1;
 }
 
 sub _is_opera_post {
@@ -22,31 +22,34 @@ sub _is_opera_ff { # opera faking as firefox
 
 sub _is_safari {
     my($self, $extra, $others) = @_;
-    return index($self->[UA_STRING],'Chrome') == -1 && (
-                    ( $extra  && index( $extra->[0], "AppleWebKit")  != -1 ) ||
-                    ( @{$others} && index( $others->[-1], "Safari" ) != -1 )
-                   );
+    my $str = $self->[UA_STRING];
+    # epiphany?
+    return                index( $str         , 'Chrome'     ) != -1 ? 0 # faker
+          :    $extra  && index( $extra->[0]  , 'AppleWebKit') != -1 ? 1
+          : @{$others} && index( $others->[-1], 'Safari'     ) != -1 ? 1
+          :                                                            0
+          ;
 }
 
 sub _is_chrome {
     my($self, $extra, $others) = @_;
     my $chx = $others->[1] || return;
-    my($chrome, $safari) = split m{\s}xms, $chx;
+    my($chrome, $safari) = split RE_WHITESPACE, $chx;
     return if ! ( $chrome && $safari);
 
-    return index($chrome,'Chrome') != -1 &&
-           index($safari,'Safari') != -1 &&
-           ( $extra  && index( $extra->[0], "AppleWebKit") != -1);
+    return              index( $chrome    , 'Chrome'     ) != -1 &&
+                        index( $safari    , 'Safari'     ) != -1 &&
+           ( $extra  && index( $extra->[0], 'AppleWebKit') != -1);
 }
 
 sub _is_ff {
     my($self, $extra) = @_;
-    return $extra && $extra->[1] && (
-                    ($extra->[1] eq 'Mozilla' && $extra->[2])
-                        ? $extra->[2] =~ RE_FIREFOX_NAMES
-                                && do { $extra->[1] = $extra->[2] }
-                        : $extra->[1] =~ RE_FIREFOX_NAMES
-                 );
+    return if ! $extra || ! $extra->[1];
+    my $moz_with_name = $extra->[1] eq 'Mozilla' && $extra->[2];
+    return $moz_with_name
+        ? $extra->[2] =~ RE_FIREFOX_NAMES && do { $extra->[1] = $extra->[2] }
+        : $extra->[1] =~ RE_FIREFOX_NAMES
+    ;
 }
 
 sub _is_gecko {
